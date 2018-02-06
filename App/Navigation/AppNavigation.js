3b612df68d637539e9a17e0c46c72963bcf67650
Unknown <adrien.arnaud@ubidreams.com>
2018-02-06 16:30:33 +0100

    container CharactersList

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,10 +1,12 @@
 import { StackNavigator } from 'react-navigation'
+import CharactersListScreen from '../Containers/CharactersListScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  CharactersListScreen: { screen: CharactersListScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
