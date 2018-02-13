4b8847ca2303266017a372e7e6e20862639ad22c
Unknown <adrien.arnaud@ubidreams.com>
2018-02-13 12:27:16 +0100

    all descriptions

--- characters.js
+++ characters.js
@@ -6,121 +6,401 @@ export default {
       id: 1,
       name: 'Iron-Man',
       avatar: Images.ironMan,
-      description: 'Anthony « Tony » Stark, alias Iron Man, est un super-héros évoluant dans l\'univers Marvel de la maison d\'édition Marvel Comics. Créé par le scénariste Stan Lee, développé par Larry Lieber, et conçu par les artistes Don Heck et Jack Kirby, le personnage de fiction apparaît pour la première fois dans le comic book Tales of Suspense #39 en mars 1963, scénarisé par Larry Lieber et dessiné par Don Heck.'
+      description: 'Anthony « Tony » Stark, alias Iron Man, est un super-héros évoluant dans l\'univers Marvel de la maison d\'édition Marvel Comics. Créé par le scénariste Stan Lee, développé par Larry Lieber, et conçu par les artistes Don Heck et Jack Kirby, le personnage de fiction apparaît pour la première fois dans le comic book Tales of Suspense #39 en mars 1963, scénarisé par Larry Lieber et dessiné par Don Heck.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 2,
       name: 'Spider-Man',
       avatar: Images.spiderMan,
-      description: ''
+      description: 'Peter Parker, alias Spider-Man (souvent écrit « Spiderman » de façon erronée), est un super-héros évoluant dans l\'univers Marvel de la maison d\'édition Marvel Comics. Créé par le scénariste Stan Lee et le dessinateur Steve Ditko, le personnage de fiction apparaît pour la première fois dans le comic book Amazing Fantasy #15 en 1962.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 3,
       name: 'Captain-America',
       avatar: Images.captainAmerica,
-      description: ''
+      description: 'Captain America est un super-héros de bande dessinée évoluant dans l\'univers Marvel de la maison d\'édition Marvel Comics. Créé par le dessinateur Jack Kirby et le scénariste Joe Simon, le personnage de fiction apparaît pour la première fois dans le comic book Captain America Comics #1 en décembre 1940.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 4,
       name: 'Hulk',
       avatar: Images.hulk,
-      description: ''
+      description: 'Robert Bruce Banner, alias Hulk, est un super-héros évoluant dans l\'univers Marvel de la maison d\'édition Marvel Comics. Créé par le scénariste Stan Lee et le dessinateur Jack Kirby, le personnage de fiction apparaît pour la première fois dans le comic book The Incredible Hulk #1 en mai 1962.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 5,
       name: 'Thor',
       avatar: Images.thor,
-      description: ''
+      description: 'Thor est une divinité et un super-héros évoluant dans l\'univers Marvel de la maison d\'édition Marvel Comics. Créé par l\'éditeur Stan Lee, le scénariste Larry Lieber et le dessinateur Jack Kirby, le personnage de fiction apparaît pour la première fois dans le comic book Journey into Mystery #83 d\'août 1962. En France, sa première apparition a lieu dans Eclipso no 15 d\'août 1971.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 6,
       name: 'Wolverine',
       avatar: Images.wolverine,
-      description: ''
+      description: 'Wolverine est un personnage de fiction appartenant à l\'univers de Marvel Comics. C\'est un super-héros, membre des X-Men et des New Avengers. Son vrai nom est James Howlett, mais ayant oublié son passé, il se fait appeler Logan.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 7,
       name: 'Ant-Man',
       avatar: Images.antMan,
-      description: ''
+      description: 'L’Homme-fourmi (« Ant-Man » en version originale) est le nom adopté par trois super-héros appartenant à l’univers Marvel de la maison d\'édition Marvel Comics, qui peuvent rapetisser et communiquer avec les fourmis.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 8,
       name: 'Apocalypse',
-      avatar: Images.wolverine,
-      description: ''
+      avatar: Images.apocalypse,
+      description: 'Apocalypse est un personnage de fiction, un super-vilain appartenant à l\'univers de Marvel Comics. Créé par Jackson Guice et Louise Simonson, il apparaît pour la première fois dans le comic book X-Factor #5 en juin 1986.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 9,
       name: 'Black Panther',
       avatar: Images.blackPanther,
-      description: ''
+      description: 'T\'Challa, alias La Panthère noire (Black Panther), est un super-héros appartenant à l\'univers Marvel de la maison d\'édition Marvel Comics. Créé par le scénariste Stan Lee et le dessinateur Jack Kirby, le personnage de fiction apparait pour la première fois dans le comic book Fantastic Four #52 en juillet 1966.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 10,
       name: 'Black Widow',
       avatar: Images.blackWidow,
-      description: ''
+      description: 'La Veuve noire (Black Widow en version originale) est le pseudonyme de deux personnages de fiction appartenant à l\'univers de Marvel Comics. Les deux sont des espionnes russes : la première (et la plus connue) est Natalia Romanova ou Natasha Romanoff.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 11,
       name: 'Daredevil',
       avatar: Images.daredevil,
-      description: ''
+      description: 'Matt Murdock, alias Daredevila, est un super-héros évoluant dans l\'univers Marvel de la maison d\'édition Marvel Comics. Créé par Stan Lee et Bill Everett, avec l\'aide de Jack Kirby et Steve Ditko, le personnage de fiction apparaît pour la première fois dans Daredevil #1 d\'avril 1964. En France, il apparaît à partir de 1970 dans le périodique Strange.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 12,
       name: 'Doctor Strange',
       avatar: Images.doctorStrange,
-      description: ''
+      description: 'Le docteur Stephen Strange, alias Docteur Strange (Doctor Strange en VO) est un super-héros appartenant à l\'univers Marvel de la maison d\'édition Marvel Comics. Créé par le scénariste Stan Lee2 et le dessinateur Steve Ditko, le personnage de fiction apparaît pour la première fois dans le comic book Strange Tales #110 en 1963.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 13,
       name: 'Emma Frost',
       avatar: Images.emmaFrost,
-      description: ''
+      description: 'Emma Frost, alias La Reine Blanche, est une ancienne super-vilaine et actuelle super-héroïne évoluant dans l\'univers Marvel de la maison d\'édition Marvel Comics. Créé par le scénariste Chris Claremont et le dessinateur John Byrne, le personnage de fiction apparaît pour la première fois dans le comic Uncanny X-Men #129 en janvier 1980.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 14,
       name: 'Hawkeye',
       avatar: Images.hawkeye,
-      description: ''
+      description: 'Clint Barton est un personnage de fiction, super-héros appartenant à l\'univers de Marvel Comics. Créé par le scénariste Stan Lee et le dessinateur Don Heck, il est apparu pour la première fois dans le comic book Tales of Suspense #57 (septembre 1964).',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 15,
       name: 'Iron-Fist',
       avatar: Images.ironFist,
-      description: ''
+      description: 'Daniel Rand, alias Iron Fist (littéralement « Poing de fer »), est un super-héros évoluant dans l\'univers Marvel de la maison d\'édition Marvel Comics. Créé par le scénariste Roy Thomas et le dessinateur Gil Kane, le personnage de fiction apparaît pour la première fois dans Marvel Premiere #15 en mai 1974. En France, ses premières aventures sont parues dans le magazine Strange à partir de juillet 1975, puis Titans jusqu\'à l’arrêt de sa première série.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 16,
       name: 'Luke Cage',
       avatar: Images.lukeCage,
-      description: ''
+      description: 'Luke Cage (né Carl Lucas), alias Power Man, est un super-héros évoluant dans l\'univers Marvel de la maison d\'édition Marvel Comics. Créé par le scénariste Archie Goodwin et le dessinateur John Romita, Sr., le personnage de fiction apparait pour la première fois dans le comic book Luke Cage, Hero for Hire #1 (traduction littérale, « Luke Cage, Héros à louer ») en juin 1972.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 17,
       name: 'Nova',
       avatar: Images.nova,
-      description: ''
+      description: 'Nova, alias Richard Rider, est un super-héros appartenant à l\'univers Marvel de la maison d\'édition américaine Marvel Comics. Créé par le scénariste Marv Wolfman et le dessinateur John Buscema, le personnage de fiction apparaît pour la première fois dans le comic book Nova #1 de septembre 1976. En France, l\'histoire est traduite dans Nova n°1 de février 1978.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 18,
       name: 'Quicksilver',
       avatar: Images.quicksilver,
-      description: ''
+      description: 'Pietro Maximoff, alias Vif-Argent (Quicksilver en version originale), est un mutant appartenant à l\'univers Marvel de la maison d\'édition Marvel Comics. Créé par le scénariste Stan Lee et le dessinateur Jack Kirby, le personnage de fiction apparaît pour la première fois dans le comic book X-Men #4 en mars 1964.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 19,
       name: 'Sif',
       avatar: Images.sif,
-      description: ''
+      description: 'Sif est un personnage de fiction, déesse appartenant à l\'univers de Marvel Comics. Créée par le scénariste Stan Lee et le dessinateur Jack Kirby, elle est apparue pour la première fois dans le comic book Journey into Mystery #102 en mars 1964. Le personnage est inspirée de la déesse scandinave Sif dans la mythologie nordique.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     },
     {
       id: 20,
       name: 'Winter Soldier',
       avatar: Images.winterSoldier,
-      description: ''
+      description: 'James Buchanan Barnes, alias « Bucky » est un super-héros évoluant dans l\'univers Marvel de la maison d\'édition Marvel Comics. Créé par Jack Kirby et Joe Simon, le personnage de fiction apparaît pour la première fois dans Captain America Comics #1 de mars 1941 dans Timely Comics.',
+      comics: {
+        available: 3,
+        items: [
+          {
+            name: 'Avengers: The Initiative (2007) #14'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)'
+          },
+          {
+            name: 'Avengers: The Initiative (2007) #15'
+          }
+        ]
+      }
     }
   ]
 }
